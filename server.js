08162c3a3a487e0b9a8d4b6e41d5d3e28e42992d
Haishi Bai <hbai@microsoft.com>
2015-06-26 13:07:24 -0700

    root response

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express=require('express')
 var app=express();
 app.use('/reading-lists', express.static('reading-lists'));
+app.get('/', function (req,res){
+  res.send('Future site of azure4fun');
+});
 var server=app.listen(80, function(){
   console.log("Server is listening at port 80");
 });
