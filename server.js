1cef5fd8566d971db4ee2cadbfd65db0448c99cf
Haishi Bai <hbai@microsoft.com>
2015-06-26 13:27:57 -0700

    updated root request

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express=require('express')
 var app=express();
 app.use('/reading-lists', express.static('reading-lists'));
 app.get('/', function (req,res){
-  res.send('Future site of azure4fun');
+  res.send('<h1>Future site of azure4fun is here</h1>');
 });
 var server=app.listen(80, function(){
   console.log("Server is listening at port 80");
