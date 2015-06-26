4fcc9c0fe3c8c4917568494d3b29bb3ef635aa34
Haishi Bai <hbai@microsoft.com>
2015-06-26 13:32:49 -0700

    reorganized folder

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express=require('express')
 var app=express();
-app.use('/reading-lists', express.static('reading-lists'));
+app.use('/reading-lists', express.static('public/reading-lists'));
 app.get('/', function (req,res){
   res.send('<h1>Future site of azure4fun is here</h1>');
 });
