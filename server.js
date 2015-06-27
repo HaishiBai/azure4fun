b64ac941a8e2e1127031e2680e35defc73b89581
Haishi Bai <hbai@microsoft.com>
2015-06-26 18:43:07 -0700

    some updates

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 var express=require('express')
 var app=express();
-app.use('/reading-lists', express.static('public/reading-lists'));
-app.get('/', function (req,res){
-  res.send('<h1>Future site of azure4fun is here</h1>');
-});
+app.use('/', express.static('public'));
 var server=app.listen(80, function(){
   console.log("Server is listening at port 80");
 });
